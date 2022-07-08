31cf4ce6247c513d45550fb1a9f2e425309399e8
K <yds06206@gmail.com>
2022-07-08 20:46:12 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,14 @@
 num = 0
 
 print('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ')
-num = int(input())
+num = input()
+
+while num != (1 or 2 or 3):
+    if (str(type(num)) != "<class 'int'>"):
+        print('정수를 입력하세요 >>')
+        num = int(input())
+    else:
+        print('1, 2, 3 중 하나를 입력하세요 >>')
+        num = int(input())
+
+int(num)
