8780a7b8ae4f903b68e05c62ca738583bf95c89e
Hanseo Kim <yds06206@gmail.com>
2022-07-09 00:11:48 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -30,27 +30,62 @@ for i in range(1, num+1):
 
 
 while True:
-    try:
-        print("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ")
-        num = int(input())
 
-    except ValueError:
-        print("정수를 입력하세요. ")
+    while True:
+        try:
+            print("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ")
+            num = int(input())
 
-    else:
-        while True:
-            if num in range(1, 4):
-                break
+        except ValueError:
+            print("정수를 입력하세요. ")
 
-            else:
-                try:
-                    print("1,2,3 중 하나를 입력하세요.")
-                    num = int(input())
-                except ValueError:
-                    print("정수를 입력하세요. ")
+        else:
+            while True:
+                if num in range(1, 4):
+                    break
+
+                else:
+                    try:
+                        print("1,2,3 중 하나를 입력하세요.")
+                        num = int(input())
+                    except ValueError:
+                        print("정수를 입력하세요. ")
+            break
         break
-    break
 
+    for i in range(len(a)+1, len(a)+num+1):
+        print('playerB : ', i)
+        a.append(i)
+        if i == 31:
+            break
 
-for i in range(len(a)+1, len(a)+num+1):
-    print('playerB : ', i)
+    while True:
+        try:
+            print("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능) : ")
+            num = int(input())
+
+        except ValueError:
+            print("정수를 입력하세요. ")
+
+        else:
+            while True:
+                if num in range(1, 4):
+                    break
+
+                else:
+                    try:
+                        print("1,2,3 중 하나를 입력하세요.")
+                        num = int(input())
+                    except ValueError:
+                        print("정수를 입력하세요. ")
+            break
+        break
+
+    for i in range(len(a)+1, len(a)+num+1):
+        print('playerA : ', i)
+        a.append(i)
+        if i == 31:
+            break
+
+    if i == 31:
+        break
